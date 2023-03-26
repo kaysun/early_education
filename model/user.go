4cbb9877f901264684d288684cd3da8562ff9ea4
kaysun <595485754@qq.com>
2023-03-26 20:54:08 +0800

    用户表

--- model/user.go
+++ model/user.go
@@ -1,14 +1,25 @@
 package model
 
+import "time"
+
 // User 用户
 type User struct {
+	// UserID 用户id
 	UserID uint64 `gorm:"column:user_id"`
 	// Name 用户名，全局唯一
 	Name string `gorm:"column:name"`
 	// Password 密码，存储密文
 	Password string `gorm:"column:password"`
+	// Nick 昵称
+	Nick string `gorm:"column:nick"`
 	// Birthday 出生日期，用于判定
 	Birthday string `gorm:"column:birthday"`
+	// Phone 手机号
+	Phone int `gorm:"column:phone"`
+	// CreateTime 创建时间
+	CreateTime time.Time `gorm:"column:create_time"`
+	// UpdateTime 修改时间
+	UpdateTime time.Time `gorm:"column:update_time"`
 }
 
 // TableName 表名
