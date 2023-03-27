12a1c13d4bd3e7973d9dbe94f9477334283b02e6
kaysun <595485754@qq.com>
2023-03-27 09:49:01 +0800

    课程model

--- model/course.go
+++ model/course.go
@@ -1,17 +1,27 @@
 package model
 
+import "time"
+
 // Course 课程
 type Course struct {
 	// CourseID 课程id
-	CourseID uint32 `json:"course_id"`
-	// Mechanism 机构
-	Mechanism string `json:"mechanism"`
-	// ClassTime 课程时间
-	ClassTime string `json:"class_time"`
+	CourseID uint32 `gorm:"column:course_id"`
 	// ClassName 课程名字
-	ClassName string `json:"class_name"`
-	// Teacher 教师
-	Teacher `json:"teacher"`
+	ClassName string `gorm:"column:name"`
+	// ClassTime 上课时间
+	ClassTime time.Time `gorm:"column:class_time"`
+	// TeacherID 教师ID
+	TeacherID int `gorm:"column:teacher_id"`
+	// MaxStudents 课程最多容纳的学生数量
+	MaxStudents int `gorm:"column:max_students"`
+	// ScheduledStudents 已经约课的学生数
+	ScheduledStudents int `gorm:"column:scheduled_students"`
+	// CourseStatus 课程状态：1-有空闲 2-约满
+	CourseStatus int `gorm:"column:course_status"`
 	// OpenState 开放状态 0-未开放 1-开放中
-	OpenState uint8 `json:"open_state"`
+	OpenState uint8 `gorm:"column:open_state"`
+	// CreateTime 创建时间
+	CreateTime time.Time `gorm:"column:create_time"`
+	// UpdateTime 修改时间
+	UpdateTime time.Time `gorm:"column:update_time"`
 }
--- transport/course/course_service.go
+++ transport/course/course_service.go
@@ -4,12 +4,13 @@ import (
 	"github.com/circle/early_education/model"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"time"
 )
 
 // List 课程列表
 func List(context *gin.Context) {
-	//timeVal1, _ := time.ParseInLocation("2006-01-02 15:04:05", "2023-01-22 09:30:00", time.Local)
-	//timeVal2, _ := time.ParseInLocation("2006-01-02 15:04:05", "2023-01-22 11:00:00", time.Local)
+	timeVal1, _ := time.ParseInLocation("2006-01-02 15:04:05", "2023-01-22 09:30:00", time.Local)
+	timeVal2, _ := time.ParseInLocation("2006-01-02 15:04:05", "2023-01-22 11:00:00", time.Local)
 
 	context.JSON(http.StatusOK, gin.H{
 		"code": 0,
@@ -17,27 +18,15 @@ func List(context *gin.Context) {
 		"data": []model.Course{
 			{
 				CourseID:  1,
-				Mechanism: "金宝贝",
-				ClassTime: "2023-01-22 09:30:00",
+				ClassTime: timeVal1,
 				ClassName: "SEL1",
-				Teacher: model.Teacher{
-					Name:      "jasmine",
-					Age:       25,
-					CourseIDs: []uint32{1},
-				},
-				OpenState: 0,
+				OpenState: 1,
 			},
 			{
 				CourseID:  2,
-				Mechanism: "金宝贝",
-				ClassTime: "2023-01-22 11:00:00",
+				ClassTime: timeVal2,
 				ClassName: "SEL2",
-				Teacher: model.Teacher{
-					Name:      "lucky",
-					Age:       23,
-					CourseIDs: []uint32{2},
-				},
-				OpenState: 0,
+				OpenState: 1,
 			},
 		},
 	})
