e5f9163dd1cfc773de7d16bb10483351ba00ede5
kaysun <595485754@qq.com>
2023-04-10 19:07:26 +0800

    初始化mysql

--- global/mysql.go
+++ global/mysql.go
@@ -1,10 +1,19 @@
 package global
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 // MysqlProxy MySQL代理
 var MysqlProxy *gorm.DB
 
 func InitMySQL() {
-	// TODO 连接mysql
+	dsn := "user:pass@tcp(127.0.0.1:3306)/education?charset=utf8mb4&parseTime=True&loc=Local"
+	var err error
+	MysqlProxy, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(fmt.Sprintf("初始化mysql失败，err=%+v", err))
+	}
 }
